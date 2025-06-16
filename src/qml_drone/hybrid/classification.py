dcaef3cd2ece5c061d0157fb2661da8c3fe1d468
2slashes <embyrne02@gmail.com>
2025-06-16 09:53:30 -0400

    remove test code from  module

--- src/qml_drone/hybrid/classification.py
+++ src/qml_drone/hybrid/classification.py
@@ -1,23 +1,9 @@
-from qml_drone.base.hybrid import (
-    train,
-    test,
-    set_qlayers,
-)
-from qml_drone.base.common import (
-    set_outputs,
-    set_model_type,
-    set_epochs,
-    set_root_dir
-)
+from qml_drone.base.hybrid import train, test
+from qml_drone.base.common import set_outputs, set_model_type
+
 
 def create():
     set_model_type("classification")
     set_outputs(5)
     train()
     test()
-
-
-set_qlayers(5)
-set_epochs(1)
-set_root_dir("../original/Radar")
-create()
--- src/qml_drone/hybrid/detection.py
+++ src/qml_drone/hybrid/detection.py
@@ -1,14 +1,5 @@
-from qml_drone.base.hybrid import (
-    train,
-    test,
-    set_qlayers,
-)
-from qml_drone.base.common import (
-    set_outputs,
-    set_model_type,
-    set_epochs,
-    set_root_dir
-)
+from qml_drone.base.hybrid import train, test
+from qml_drone.base.common import set_outputs, set_model_type
 
 
 def create():
@@ -16,9 +7,3 @@ def create():
     set_outputs(2)
     train()
     test()
-
-
-set_qlayers(5)
-set_epochs(1)
-set_root_dir("../original/Radar/binary")
-create()
