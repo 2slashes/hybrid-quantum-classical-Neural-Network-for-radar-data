2a0fa721c2173002a2ce0260eb65982b6f802faf
2slashes <embyrne02@gmail.com>
2025-06-24 13:43:33 -0400

    fix errors in train and test functions

--- src/qmldrone/jobs/_base.py
+++ src/qmldrone/jobs/_base.py
@@ -1,15 +1,20 @@
-from qmldrone.common import common_train, common_test, get_paths, dataloader
+from ..metrics._base import train as common_train, test as common_test
+from ..io._input import dataloader
 import torchvision.datasets as ds
 import torch
 import os
 
 
-def train(conf, model_builder, model_dir: str="../original/Radartwo_sided_models", train_data_dir: str="../original/Radartwo_sided_train_data"):
-
+def train(
+    conf,
+    model_builder,
+    model_dir: str = "../original/Radartwo_sided_models",
+    train_data_dir: str = "../original/Radartwo_sided_train_data",
+):
     os.system(f"mkdir -p {model_dir}")
     for snr in conf["snr"]:
         cur_model_path = f"{model_dir}/{conf['model_name']}-{snr}.pt"
-        trainset_root = f"{paths['train_data_dir']}/{conf['f_s']}fs/{snr}SNR"
+        trainset_root = f"{train_data_dir}/{conf['f_s']}fs/{snr}SNR"
         trainds = ds.DatasetFolder(trainset_root, dataloader, extensions=("npy",))
         trainLoader = torch.utils.data.DataLoader(
             trainds, conf["batch_size"], shuffle=True, num_workers=2
@@ -19,13 +24,18 @@ def train(conf, model_builder, model_dir: str="../original/Radartwo_sided_models
         net = model_builder(conf)
         common_train(conf, net, cur_model_path, trainLoader)
 
-def test(conf, model_builder):
-    paths = get_paths()
 
-    os.system(f"mkdir -p {paths['plot_dir']}")
+def test(
+    conf,
+    model_builder,
+    model_dir: str = "../original/Radartwo_sided_models",
+    plot_dir: str = "../original/Radartwo_sided_plots",
+    test_data_dir: str = "../original/Radartwo_sided_test_data",
+):
+    os.system(f"mkdir -p {plot_dir}")
     for snr in conf["snr"]:
-        cur_model_path = f"{paths['model_dir']}/{conf['model_name']}-{snr}.pt"
-        testset_root = f"{paths['test_data_dir']}/{conf['f_s']}fs/{snr}SNR"
+        cur_model_path = f"{model_dir}/{conf['model_name']}-{snr}.pt"
+        testset_root = f"{test_data_dir}/{conf['f_s']}fs/{snr}SNR"
         testds = ds.DatasetFolder(testset_root, dataloader, extensions=("npy",))
         testLoader = torch.utils.data.DataLoader(
             testds, conf["batch_size"], shuffle=True, num_workers=2
@@ -34,6 +44,4 @@ def test(conf, model_builder):
         print(f"SNR: {snr} dB")
 
         net = model_builder(conf)
-        common_test(
-            conf, net, snr, cur_model_path, testLoader, plot_dir=paths["plot_dir"]
-        )
+        common_test(conf, net, snr, cur_model_path, testLoader, plot_dir=plot_dir)
