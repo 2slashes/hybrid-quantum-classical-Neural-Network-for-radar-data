b70dd4e5cc2e7e9292975cdbd24ea1c82510f894
2slashes <embyrne02@gmail.com>
2025-07-07 13:31:40 -0400

    remove comments

--- src/qmldrone/jobs/_base.py
+++ src/qmldrone/jobs/_base.py
@@ -2,58 +2,8 @@ from ..metrics._base import test_metrics
 from ..io._input import dataloader
 import torchvision.datasets as ds
 import torch
-import os
 import torch.nn as nn
 
-
-# def train(
-#     model_builder,
-#     conf,
-#     num_outputs,
-#     device,
-#     model_dir: str,
-#     train_data_dir: str,
-#     disable_min_epochs=True,
-# ):
-#     os.system(f"mkdir -p {model_dir}")
-#     for snr in conf["snr"]:
-        
-#         trainset_root = f"{train_data_dir}/{conf['f_s']}fs/{snr}SNR"
-#         trainds = ds.DatasetFolder(trainset_root, dataloader, extensions=("npy",))
-#         trainLoader = torch.utils.data.DataLoader(
-#             trainds, conf["batch_size"], shuffle=True, num_workers=2
-#         )
-
-#         print(f"SNR: {snr} dB")
-#         net = model_builder(num_outputs)
-
-#         net = net.to(device)
-#         optim = torch.optim.AdamW(net.parameters(), lr=conf["learning_rate"])
-#         loss_fn = nn.CrossEntropyLoss().to(device)
-#         for x in range(conf["epochs"]):
-#             net.train()
-
-#             for i, data in enumerate(trainLoader):
-#                 # plot_spectrogram(torch.squeeze(data[0]))
-#                 inputs, labels = data
-#                 inputs = inputs.to(device)
-#                 labels = labels.to(device)
-#                 optim.zero_grad()
-
-#                 outputs = net(inputs.float())
-#                 loss = loss_fn(outputs, labels)
-#                 loss.backward()
-#                 optim.step()
-#                 loss_val = loss.item()
-
-#             print("Train Epoch: {} Loss: {:.6f}".format(x, loss_val))
-#             if not disable_min_epochs and x > conf["min_epochs"] and loss_val < conf["loss_threshold"]:
-#                 break
-
-#         if conf["save_model"] is True:
-#             cur_model_path = f"{model_dir}/{conf['model_name']}-{snr}.pt"
-#             print(f"Saving model state to {cur_model_path}")
-#             torch.save(net.state_dict(), cur_model_path)
 
 def train(
     net,
@@ -90,13 +40,12 @@ def train(
             loss_val = loss.item()
 
         print("Train Epoch: {} Loss: {:.6f}".format(x, loss_val))
-        if not disable_min_epochs and x > conf["min_epochs"] and loss_val < conf["loss_threshold"]:
+        if (
+            not disable_min_epochs
+            and x > conf["min_epochs"]
+            and loss_val < conf["loss_threshold"]
+        ):
             break
-
-    # if conf["save_model"] is True:
-    #     cur_model_path = f"{model_dir}/{conf['model_name']}-{snr}.pt"
-    #     print(f"Saving model state to {cur_model_path}")
-    #     torch.save(net.state_dict(), cur_model_path)
 
 
 def test(
--- src/qmldrone/models/_base.py
+++ src/qmldrone/models/_base.py
@@ -3,10 +3,54 @@ from ..jobs._base import train, test
 import os
 import torch
 
+
 def create_base(conf_path: str, classifier):
     conf, paths, classes = load_yaml(conf_path)
     num_outputs = len(classes)
     device = get_device()
+    train_conf, test_conf = setup_train_and_test_conf(conf)
+
+    os.system(f"mkdir -p {paths['model_dir']}")
+    os.system(f"mkdir -p {paths['plot_dir']}")
+
+    metrics_conf = dict(
+        (key, conf[key])
+        for key in (
+            "model_type",
+            "plot_confusion",
+        )
+    )
+
+    nets = {}
+    for snr in conf["snr"]:
+        nets[snr] = classifier(num_outputs).to(device)
+        train(
+            nets[snr],
+            train_conf,
+            device,
+            snr,
+            paths["model_dir"],
+            paths["train_data_dir"],
+            conf["disable_min_epochs"],
+        )
+        test(
+            nets[snr],
+            test_conf,
+            metrics_conf,
+            classes,
+            device,
+            snr,
+            paths["model_dir"],
+            paths["plot_dir"],
+            paths["test_data_dir"],
+        )
+        if conf["save_model"] is True:
+            cur_model_path = f"{paths['model_dir']}/{conf['model_name']}-{snr}.pt"
+            print(f"Saving model state to {cur_model_path}")
+            torch.save(nets[snr].state_dict(), cur_model_path)
+
+
+def setup_train_and_test_conf(conf):
     train_conf = {}
     test_conf = {}
     if conf["save_model"]:
@@ -61,63 +105,4 @@ def create_base(conf_path: str, classifier):
                 "plot_confusion",
             )
         )
-
-    # train(
-    #     classifier,
-    #     train_conf,
-    #     num_outputs,
-    #     device,
-    #     paths["model_dir"],
-    #     paths["train_data_dir"],
-    #     conf["disable_min_epochs"]
-    # )
-    # test(
-    #     classifier,
-    #     test_conf,
-    #     num_outputs,
-    #     classes,
-    #     device,
-    #     paths["model_dir"],
-    #     paths["plot_dir"],
-    #     paths["test_data_dir"],
-    # )
-
-    os.system(f"mkdir -p {paths['model_dir']}")
-    os.system(f"mkdir -p {paths['plot_dir']}")
-
-    metrics_conf = dict(
-        (key, conf[key])
-        for key in (
-            "model_type",
-            "plot_confusion",
-        )
-    )
-    
-    nets = {}
-    for snr in conf["snr"]:
-        nets[snr] = (classifier(num_outputs).to(device))
-        train(
-            nets[snr],
-            train_conf,
-            device,
-            snr,
-            paths["model_dir"],
-            paths["train_data_dir"],
-            conf["disable_min_epochs"]
-        )
-        test(
-            nets[snr],
-            test_conf,
-            metrics_conf,
-            classes,
-            device,
-            snr,
-            paths["model_dir"],
-            paths["plot_dir"],
-            paths["test_data_dir"],
-        )
-        if conf["save_model"] is True:
-            cur_model_path = f"{paths['model_dir']}/{conf['model_name']}-{snr}.pt"
-            print(f"Saving model state to {cur_model_path}")
-            torch.save(nets[snr].state_dict(), cur_model_path)
-
+    return train_conf, test_conf
